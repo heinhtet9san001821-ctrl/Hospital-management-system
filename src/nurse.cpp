95c74336fb30b4eea8cac4c485c30be8d942393f
April Win <aprilwin371@gmail.com>
2026-08-09 22:45:04 +0630

    feat: Implement Nurse Constructor

---
+++ src/nurse.cpp
@@ -0,0 +1,14 @@
+#include "nurse.h"
+#include <iostream>
+
+namespace hms {
+
+Nurse::Nurse(const std::string& id,
+             const std::string& name,
+             int age,
+             const std::string& gender,
+             const std::string& phoneNumber,
+             const std::string& nurseId,
+             const std::string& assignedWard,
+             const std::string& shiftTime)
+   
