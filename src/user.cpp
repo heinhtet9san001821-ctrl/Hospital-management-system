46c95ddcc78b5fa7ab2560d8e6c0713b47841162
Akari1235 <mail.akarigugi.org>
2026-08-09 23:17:33 +0630

    Add password authentication

--- src/user.cpp
+++ src/user.cpp
@@ -9,3 +9,18 @@ namespace hms
                const std::string &password,
                const std::string &role)
         : username_(username), passwordHash_(hashPassword(password)), role_(role) {}
+    std::string User::hashPassword(const std::string &password)
+    {
+
+        std::hash<std::string> hasher;
+        std::size_t hashed = hasher(password);
+
+        std::ostringstream oss;
+        oss << hashed;
+        return oss.str();
+    }
+
+    bool User::authenticate(const std::string &password) const
+    {
+        return passwordHash_ == hashPassword(password);
+    }
