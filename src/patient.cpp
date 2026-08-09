9cfd64bc586fccee34a06ecc1962797f23eb2954
Hsu Yati Htet <hsuyatihtet704@gmail.com>
2026-08-09 22:49:43 +0630

    Implementing getPatientId

--- src/patient.cpp
+++ src/patient.cpp
@@ -21,40 +21,5 @@ Patient::Patient(const std::string& id,
       isEmergency_(isEmergency) {}
 
 const std::string& Patient::getPatientId() const { return patientId_; }
-const std::string& Patient::getMedicalHistory() const { return medicalHistory_; }
-const std::string& Patient::getTreatmentType() const { return treatmentType_; }
-int Patient::getAssignedRoomNo() const { return assignedRoomNo_; }
-bool Patient::isEmergency() const { return isEmergency_; }
 
-void Patient::setMedicalHistory(const std::string& medicalHistory) {
-    medicalHistory_ = medicalHistory;
-}
 
-void Patient::setTreatmentType(const std::string& treatmentType) {
-    treatmentType_ = treatmentType;
-}
-
-void Patient::setAssignedRoomNo(int roomNo) {
-    assignedRoomNo_ = roomNo;
-}
-
-void Patient::setEmergency(bool isEmergency) {
-    isEmergency_ = isEmergency;
-}
-
-void Patient::displayDetails() const {
-    std::cout << "----- Patient Details -----\n";
-    std::cout << "Patient ID      : " << patientId_ << "\n";
-    std::cout << "Person ID       : " << id_ << "\n";
-    std::cout << "Name            : " << name_ << "\n";
-    std::cout << "Age             : " << age_ << "\n";
-    std::cout << "Gender          : " << gender_ << "\n";
-    std::cout << "Phone Number    : " << phoneNumber_ << "\n";
-    std::cout << "Medical History : " << medicalHistory_ << "\n";
-    std::cout << "Treatment Type  : " << treatmentType_ << "\n";
-    std::cout << "Assigned Room   : " << assignedRoomNo_ << "\n";
-    std::cout << "Emergency Case  : " << (isEmergency_ ? "Yes" : "No") << "\n";
-    std::cout << "----------------------------\n";
-}
-
-} // namespace hms
