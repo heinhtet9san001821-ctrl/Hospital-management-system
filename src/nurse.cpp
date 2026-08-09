9498d31a3e381a75a6ed3e3484c07e31870c2298
April Win <aprilwin371@gmail.com>
2026-08-09 22:48:03 +0630

    feat: Implement Nurse Functions

--- src/nurse.cpp
+++ src/nurse.cpp
@@ -11,4 +11,20 @@ Nurse::Nurse(const std::string& id,
              const std::string& nurseId,
              const std::string& assignedWard,
              const std::string& shiftTime)
-   
+        : Person(id, name, age, gender, phoneNumber),
+      nurseId_(nurseId),
+      assignedWard_(assignedWard),
+      shiftTime_(shiftTime) {}
+
+const std::string& Nurse::getNurseId() const { return nurseId_; }
+const std::string& Nurse::getAssignedWard() const { return assignedWard_; }
+const std::string& Nurse::getShiftTime() const { return shiftTime_; }
+
+void Nurse::setAssignedWard(const std::string& assignedWard) {
+    assignedWard_ = assignedWard;
+}
+
+void Nurse::setShiftTime(const std::string& shiftTime) {
+    shiftTime_ = shiftTime;
+}
+
