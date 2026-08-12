79b1e9a89d7aac6aca50115fd8cd5505c6068d33
Hein Htet San <heinhtet9san001821@gmail.com>
2026-08-12 23:27:42 +0630

    Adding the new data feature

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include "hospital_manager.h"
 #include "patient.h"
 #include "doctor.h"
 #include "nurse.h"
@@ -5,40 +6,19 @@
 #include <iostream>
 
 int main() {
-    std::cout << "\033[1;32m[SUCCESS] HMS Core OOP Modules Phase 1 Linked Perfectly!\033[0m\n\n";
+    std::cout << "\033[1;32m[START] Phase 2: Testing Full CSV Database Core System...\033[0m\n\n";
 
-    // Testing the patient obj
-    hms::Patient p1("P001", "Aung Aung", 25, "Male", "091234567", "PAT_001", "No history", "General", 101, true);
-    p1.displayDetails();
-    std::cout << "\n";
+    hms::HospitalManager manager;
 
-    // Testing the doctor obj
-    hms::Doctor d1("D001", "Dr. Thura", 40, "Male", "097654321", "DOC_001", "Cardiologist", 302, true);
-    d1.displayDetails();
-    std::cout << "\n";
+    // ==========================================
+    // FEATURE 1: Adding the new data (CRUD Test)
+    // ==========================================
+    std::cout << "Step 1: Adding Patient, Doctor, Nurse, and User to memory...\n";
+    manager.addPatient(hms::Patient("P001", "Aung Aung", 25, "Male", "091234567", "PAT_001", "No history", "General", 101, true));
+    manager.addDoctor(hms::Doctor("D001", "Dr. Thura", 40, "Male", "097654321", "DOC_001", "Cardiologist", 302, true));
+    manager.addNurse(hms::Nurse("N001", "Daw Nu Nu", 30, "Female", "094445556", "NUR_001", "ICU Ward", "Night Shift"));
+    manager.addUser(hms::User("admin", "hunter2", "Owner"));
 
-    // Testing the nurse obj
-    // ID, Name, Age, Gender, Phone, NurseId, AssignedWard, ShiftTime
-    hms::Nurse n1("N001", "Daw Nu Nu", 30, "Female", "094445556", "NUR_001", "ICU Ward", "Night Shift");
-    n1.displayDetails();
-    std::cout << "\n";
-
-    // User account and encryption test
-    hms::User u1("admin", "secure_password_hash_xyz", "Owner");
-    std::cout << "\033[1;36m-----------------------------------------\033[0m\n";
-    std::cout << "\033[1;35m          USER ACCOUNT SYSTEM            \033[0m\n";
-    std::cout << "\033[1;36m-----------------------------------------\033[0m\n";
-    std::cout << "Username     : " << u1.getUsername() << "\n";
-    std::cout << "Access Role  : \033[1;33m[" << u1.getRole() << "]\033[0m\n";
-    
-    // Login Verification
-    std::cout << "Authentication: ";
-    if (u1.authenticate("secure_password_hash_xyz")) {
-        std::cout << "\033[1;32m[ACCESS GRANTED - VERIFIED]\033[0m\n";
-    } else {
-        std::cout << "\033[1;31m[ACCESS DENIED]\033[0m\n";
-    }
-    std::cout << "\033[1;36m-----------------------------------------\033[0m\n";
 
     return 0;
 }
